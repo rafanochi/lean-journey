6718c40eb229bf526f51d3159ff6c92272814dcc
rafanochi <zawkindev@gmail.com>
2025-11-02 21:38:13 +0500

    simple commands

--- main.lean
+++ main.lean
@@ -1,2 +1,18 @@
-#eval (4+3 : Int)
-#eval String.append "Hello " "World"
+def m : Nat := 1
+def n : Nat := 2
+def isNice : Bool := false
+def amINice : Bool := false
+
+#check m
+
+#check isNice
+
+#check n + 0
+
+#check m * (n+0)
+
+#check isNice && amINice
+
+#check true
+
+#eval 5*4
